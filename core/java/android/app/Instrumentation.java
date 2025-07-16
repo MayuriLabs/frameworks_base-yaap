e827820b1cc9e1303ee033b6f127a62ef93c31c7
wulan17 <wulan17@komodos.id>
2025-07-16 19:34:09 +0700

    base: Completely drop PixelPropsUtils

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -65,7 +65,6 @@ import android.view.Window;
 import android.view.WindowManagerGlobal;
 
 import com.android.internal.content.ReferrerIntent;
-import com.android.internal.util.yaap.PixelPropsUtils;
 
 import java.io.File;
 import java.lang.annotation.Retention;
@@ -128,7 +127,6 @@ public class Instrumentation {
             UiAutomation.FLAG_DONT_USE_ACCESSIBILITY})
     public @interface UiAutomationFlags {};
 
-    private static PixelPropsUtils mPixelPropsUtils = null;
 
     private final Object mSync = new Object();
     private ActivityThread mThread = null;
@@ -1348,10 +1346,6 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
-        PixelPropsUtils ppu = PixelPropsUtils.getInstance(context);
-        if (ppu != null) {
-            ppu.setProps(context.getPackageName());
-        }
         return app;
     }
     
@@ -1369,10 +1363,6 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
-        PixelPropsUtils ppu = PixelPropsUtils.getInstance(context);
-        if (ppu != null) {
-            ppu.setProps(context.getPackageName());
-        }
         return app;
     }
 
--- keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
+++ keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
@@ -48,7 +48,6 @@ import android.system.keystore2.ResponseCode;
 import android.util.Log;
 
 import com.android.internal.annotations.VisibleForTesting;
-import com.android.internal.util.yaap.PixelPropsUtils;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -194,14 +193,9 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
 
     @Override
     public Certificate[] engineGetCertificateChain(String alias) {
-        if (PixelPropsUtils.getIsEnabled()) {
-            if (PixelPropsUtils.getIsFinsky()) {
-                throw new UnsupportedOperationException("Blocking safetynet attestation for finsky");
-            }
-            for (StackTraceElement ste : Thread.currentThread().getStackTrace()) {
-                if (ste.getClassName().contains("DroidGuard")) {
-                    throw new UnsupportedOperationException("Blocking safetynet attestation");
-                }
+        for (StackTraceElement ste : Thread.currentThread().getStackTrace()) {
+            if (ste.getClassName().contains("DroidGuard")) {
+                throw new UnsupportedOperationException("Blocking safetynet attestation");
             }
         }
 
